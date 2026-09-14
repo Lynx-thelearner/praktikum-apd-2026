6b991e15f3d06e0bbe8cb5c7e15f71695a4d06fe
Lynx-thelearner <daffaauliad@gmail.com>
2026-09-14 12:16:41 +0800

    pertemuan 1

---
+++ pertemuan-1.py
@@ -0,0 +1,34 @@
+# a = 5.5
+# b = 10.9
+# c = a + b
+
+# print(c)
+
+
+nama = "Larry"
+hobi = "Ngoding"
+
+print(nama, hobi)
+
+print('''Halo saya Rendy
+    Saya Suka belajar coding
+    Hari ini saya belajar bahasa pemrograman Python''')
+
+teks = "Rahmat Flowchart"
+
+print(teks[6])
+
+mata_kuliah = ["APD", "Logika Matematika", "Kalkulus"]
+
+print(mata_kuliah[0])
+print(mata_kuliah[1])
+print(mata_kuliah[2])
+
+username = input("Masukkan username:")
+print("Username:", username)
+
+a = 20
+b = 10
+c = a * b
+
+print(c)
