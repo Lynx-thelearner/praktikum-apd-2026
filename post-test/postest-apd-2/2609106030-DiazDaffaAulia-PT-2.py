f5b9a6bc58202817bc558a199dd8e39aaec9596b
Lynx-thelearner <daffaauliad@gmail.com>
2026-09-19 12:55:49 +0800

    Changing variables names

--- post-test/postest-apd-2/2609106030-DiazDaffaAulia-PT-2.py
+++ post-test/postest-apd-2/2609106030-DiazDaffaAulia-PT-2.py
@@ -1,5 +1,14 @@
-bagasi = [12, 18, 7, 15, 20, 10]
+bagasi_1 = 12
+bagasi_2 = 18
+bagasi_3 = 7
+bagasi_4 = 15
+bagasi_5 = 20
+bagasi_6 = 10
+
+bagasi = [bagasi_1, bagasi_2, bagasi_3, bagasi_4, bagasi_5, bagasi_6]
+
 total_berat_akhir = bagasi[0] + bagasi[1] + bagasi[2] + bagasi[3] + bagasi[4] + bagasi[5]
+
 total_bayar = total_berat_akhir * 5 / 100
 
 rata_rata = total_berat_akhir / len(bagasi)
